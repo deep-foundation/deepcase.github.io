38ac16ea9a85b6d6f638e771f9d9d8e1b898ed25
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-05-15 22:30:07 +0000

    fix prod

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  basePath: isProd ? '/store' : '',
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deep.foundation/gh-pages/' : '',
+  basePath: isProd ? '' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deep.foundation/main/' : '',
 });
