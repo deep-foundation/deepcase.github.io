591741c258a71719cb52368df90f21bb8b435743
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-08-23 15:57:25 +0000

    waitlist

--- pages/pre-order.tsx
+++ pages/pre-order.tsx
@@ -19,6 +19,9 @@ import _ from 'lodash';
 import { Provider } from '../imports/provider';
 import { useEffect } from 'react';
 import { useAnalitics } from '../imports/analitics';
+import dynamic from "next/dynamic";
+
+const Waitlist = dynamic(() => import("waitlistapi").then(w => w.Waitlist), { ssr: false });
 
 const whyNoVariants = [
   {
@@ -64,176 +67,13 @@ export default function Page(props) {
             <Typography variant="h2">Pre-order</Typography>
           </Grid>
           <Grid item xs={12} style={{ height: 50 }} />
-          <Grid item xs={12}>
-            <Typography variant="h4">Resourses</Typography>
-            <Typography><a href="#">medium</a></Typography>
-            <Typography><a href="#">habr</a></Typography>
-          </Grid>
-          <Grid item xs={12} style={{ height: 50 }} />
-          <Grid item xs={12}>
-            <Typography variant="h4">Want to use Deep solutions?</Typography>
-            <Grid container justify="center" alignItems="center" spacing={1}>
-              <Grid item><ButtonExplotion variant="outlined" onClick={() => {
-                setWant(want === 1 ? 0 : 1);
-                event('want-yes');
-              }} disabled={want === 1}>yes ({counters.wantYes})</ButtonExplotion></Grid>
-              <Grid item><Button variant="outlined" onClick={() => {
-                setWant(want === 2 ? 0 : 2);
-                event('want-no');
-              }} disabled={want === 2}>no ({counters.wantNo})</Button></Grid>
-            </Grid>
-            {want === 1 && <>
-              <Grid container justify="center" alignItems="center" spacing={1}>
-                <Grid item xs={12} style={{ height: 50 }} />
-                <Grid item xs={12}>
-                  <Typography variant="h4">Want to subscribe?</Typography>
-                </Grid>
-                <Grid item>
-                  <ButtonGroup variant="outlined">
-                    <Button color={needNews === 1 ? 'primary' : 'default'} onClick={() => {
-                      setNeedNews(1);
-                      event('need-news-1');
-                    }}>all ({counters.needNews1})</Button>
-                    <Button color={needNews === 2 ? 'primary' : 'default'} onClick={() => {
-                      setNeedNews(2);
-                      event('need-news-2');
-                    }}>only release ({counters.needNews2})</Button>
-                    <Button color={needNews === 3 ? 'primary' : 'default'} onClick={() => {
-                      setNeedNews(3);
-                      event('need-news-3');
-                    }}>no ({counters.needNews3})</Button>
-                  </ButtonGroup>
-                </Grid>
-                <Grid item xs={12}>
-                  <Collapse in={needNews !== 3}>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      multiline
-                      placeholder='email@test.com'
-                      disabled={needNews === 3}
-                      value={email}
-                      onChange={e => setEmail(e.target.value)}
-                    />
-                  </Collapse>
-                </Grid>
-              </Grid>
-              {needNews !== 0 && <>
-                <Grid container justify="center" alignItems="center" spacing={1}>
-                  <Grid item xs={12} style={{ height: 50 }} />
-                  <Grid item xs={12}>
-                    <Typography variant="h4">What is your favorite plan?</Typography>
-                    <Typography>Deep.Case is boxed solution with community and enterprise edition, but without devops and deep realtime optimization.</Typography>
-                    <Typography>Deep.Space is cloud solution multiple plans. International union associative database with devops and deep realtime optimization.</Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Card variant="outlined">
-                      <CardContent style={{ paddingBottom: 16 }}>
-                        <Typography variant="h5">Hobby plan</Typography>
-                        <Typography>free</Typography>
-                        <Grid container justify="center" alignItems="center" spacing={1}>
-                          <Grid item xs={12}><Divider/></Grid>
-                          <Grid item xs={12}>
-                            <Typography>base platform</Typography>
-                            <Typography>easy to develop</Typography>
-                            <Typography>useful for fast prototyping</Typography>
-                            <Typography>multilanguage and multistack</Typography>
-                            <Typography>open source comunity edition</Typography>
-                          </Grid>
-                          <Grid item xs={12}><Divider/></Grid>
-                          <Grid item><ButtonExplotion variant="outlined" onClick={() => {
-                            setWantPlan(1);
-                            setBuy(false);
-                            event('want-plan-1');
-                          }} color={wantPlan === 1 ? 'primary' : 'default'}>want ({counters.wantPlan1})</ButtonExplotion></Grid>
-                        </Grid>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Grid container justify="center" alignItems="center" spacing={1}>
-                      <Grid item xs={12}>
-                        <Card variant="outlined">
-                          <CardContent style={{ paddingBottom: 16 }}>
-                            <Typography variant="h5">Startup plan</Typography>
-                            <Typography>19$ <s>39$</s></Typography>
-                            <Grid container justify="center" alignItems="center" spacing={1}>
-                              <Grid item xs={12}><Divider/></Grid>
-                              <Grid item xs={12}>
-                                <Typography>international kuber cluster</Typography>
-                                <Typography>autobalance</Typography>
-                                <Typography>regional law store&calculating</Typography>
-                                <Typography>pgsql smart optimization</Typography>
-                                <Typography>runners/handlers autobalance</Typography>
-                              </Grid>
-                              <Grid item xs={12}><Divider/></Grid>
-                              <Grid item><ButtonExplotion variant="outlined" color={buy ? 'primary' : 'default'} onClick={() => {
-                                setWantPlan(2);
-                                setBuy(true);
-                                event('buy-plan-2');
-                              }}>buy ({counters.wantBuy})</ButtonExplotion></Grid>
-                              <Grid item><ButtonExplotion variant="outlined" onClick={() => {
-                                setWantPlan(2);
-                                event('want-plan-2');
-                              }} color={wantPlan === 2 ? 'primary' : 'default'}>want ({counters.wantPlan2})</ButtonExplotion></Grid>
-                            </Grid>
-                          </CardContent>
-                        </Card>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                  {buy && <>
-                    <Grid item xs={12} style={{ height: 50 }} />
-                    <Grid item xs={12} sm={6}>
-                      <Card variant="outlined">
-                        <CardContent style={{ paddingBottom: 16 }}>
-                          <Typography variant="h5">payment</Typography>
-                          <Typography>{planLength * 19}$ <s>{planLength * 39}$</s></Typography>
-                          <Typography>{planLength} {planLength > 1 ? 'months' : 'month'}</Typography>
-                          <Slider
-                            defaultValue={1}
-                            valueLabelDisplay="auto"
-                            step={1}
-                            marks
-                            min={1}
-                            max={24}
-                            onChange={(e, v) => setPlanLength(+v)}
-                            value={planLength}
-                          />
-                          <Typography>manually activated after release</Typography>
-                          <Typography variant="body2">return possible</Typography>
-                          <ButtonExplotion fullWidth variant="outlined" onClick={() => {
-                            event('pay');
-                          }}>
-                            pay ({counters.wantPay})
-                          </ButtonExplotion>
-                        </CardContent>
-                      </Card>
-                    </Grid>
-                  </>}
-                </Grid>
-              </>}
-            </>}
-            {want === 2 && <>
-              <Grid item xs={12} style={{ height: 50 }} />
-              <Typography variant="h4">Why?</Typography>
-              <FormControl component="fieldset">
-                <FormGroup>
-                  {whyNoVariants.map(variant => (
-                    <FormControlLabel
-                      control={<Checkbox checked={!!whyNo[variant.key]} onChange={() => setWhyNo({ ...whyNo, [variant.key]: !whyNo[variant.key] })} name={variant.key} />}
-                      label={variant.label}
-                    />
-                  ))}
-                </FormGroup>
-              </FormControl>
-              <TextField
-                fullWidth
-                variant="outlined"
-                multiline
-                placeholder='custom reason'
-              />
-            </>}
+          <Grid item xs={12}><>
+            <Waitlist
+              api_key="BBNAIR"
+              waitlist_link="https://deep.foundation/pre-order"
+              joinWaitlistHeading="Support us and make pre-order."
+            />
+          </>
           </Grid>
           <Grid item xs={12} style={{ height: '30vh' }} />
         </Grid>
