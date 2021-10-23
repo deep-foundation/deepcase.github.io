b758bd16977026e9ed2811a7dba4bd8798348557
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-10-23 01:43:32 +0000

    gh-pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b898be77467a21b05705";
+/******/ 	var hotCurrentHash = "b3115cddb1cbc110b087";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
