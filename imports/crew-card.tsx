58045bb048c84dd98c25dade3fd5ed49f66101e0
Anastasia <llingveri@gmail.com>
2022-01-17 01:06:22 +0300

    https://github.com/deep-foundation/deepcase.github.io/issues/6

--- imports/crew-card.tsx
+++ imports/crew-card.tsx
@@ -4,6 +4,15 @@ import React from 'react';
 const useStyles = makeStyles(theme => ({
   container: {
     background: 'transparent', 
+    overflow: 'hidden',
+    boxSizing: 'border-box',
+    boxShadow: '0 0 1px 1px #393d40, 0 0 1px 2px rgb(0 0 0 / 16%), 0 0 2px 3px rgb(0 0 0 / 14%), 0 0 4px 5px rgb(0 0 0 / 12%)',
+    borderRadius: '50%',
+    width: ' calc(200px + 0.5vmax)',
+    height: ' calc(200px + 0.5vmax)',
+    display: 'flex',
+    alignItems: 'self-start',
+    margin: '0 auto',
   },
   img: {
     width: '100%',
