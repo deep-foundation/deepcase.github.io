d5b15cfcd055a83bcb8abb527be4789d02c0ece4
Anastasia <llingveri@gmail.com>
2022-01-25 02:39:20 +0300

    https://github.com/deep-foundation/deepcase.github.io/issues/11

--- next.config.js
+++ next.config.js
@@ -8,4 +8,22 @@ module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   basePath: isProd ? '' : '',
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deepcase.github.io/main/docs/' : '',
+
+  i18n: {
+    locales: ['en-US', 'ru'],
+    defaultLocale: 'ru',
+    // domains: [
+    //   {
+    //     domain: 'https://deep.foundation/',
+    //     defaultLocale: 'ru',
+    //   },
+    //   {
+    //     domain: 'https://deep.foundation/',
+    //     defaultLocale: 'en-US',
+    //     // an optional http field can also be used to test
+    //     // locale domains locally with http instead of https
+    //     http: true,
+    //   },
+    // ],
+  },
 });
