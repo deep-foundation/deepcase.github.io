bb517c850bb32c0e907b22aa483ddace24f84baf
Anastasia <llingveri@gmail.com>
2022-03-09 15:13:14 +0300

    gh-pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b8795113b43f823be882";
+/******/ 	var hotCurrentHash = "e0ca226de44d57e9fb0e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
