8ed06f80a1ded90c4e9203a712fbf8953697c5ff
Anastasia <llingveri@gmail.com>
2022-09-20 13:24:57 +0300

    deep case frame

---
+++ pages/deep-frame.tsx
@@ -0,0 +1,37 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { Box } from '../imports/framework';
+import { Provider } from '../imports/provider';
+import { linksTheme } from '../imports/theme/build';
+
+import { useScroll } from 'framer-motion';
+import { DeepFrame } from '../imports/deep-case-frame/deep-frame';
+
+
+export default function Page () {
+  return (
+    <ChakraProvider theme={linksTheme}>
+      <Provider>
+        <PageContent />
+      </Provider>
+    </ChakraProvider>
+  );
+};
+
+export const PageContent = React.memo(() => {
+  const { scrollY } = useScroll();
+
+  useEffect(() => {
+    return scrollY.onChange((latest) => {
+      console.log("Page scroll: ", latest)
+    })
+  }, [])
+  
+  
+  return (<Box as='main' pos='relative'>
+      {/* <Menu sx={{ width: '100%', bg: 'transparent', p: '4'}} /> */}
+      {/* <AdaptiveSpace unit={{sm: '2rem', md: '6rem'}} /> */}
+      <DeepFrame />
+    </Box>
+  );
+});
