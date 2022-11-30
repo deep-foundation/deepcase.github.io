3513d955aa6427438e21d80842f76074eace2bfc
Anastasia <llingveri@gmail.com>
2022-11-30 23:33:31 +0300

    blob experiments

--- pages/blob.tsx
+++ pages/blob.tsx
@@ -5,10 +5,9 @@ import { Provider } from '../imports/provider';
 import { coreTheme } from '../imports/theme/build';
 
 import { useScroll } from 'framer-motion';
+import { MotionModal } from '../imports/motion-modal';
 import { Blob } from '../imports/form-blob';
 import { InvitationToTheCave } from '../imports/invitation-to-the-cave';
-import { Backdrop } from '../imports/backdrop';
-import { TalksFormCust } from '../imports/invitation-modal';
 // import { motion, useScroll } from "framer-motion";
 
 
@@ -25,6 +24,7 @@ export default function Page () {
 export const PageContent = React.memo(() => {
 
   const { scrollY } = useScroll();
+  const [ contentReplacement, setContentReplacement ] = useState(false);
 
   useEffect(() => {
     return scrollY.onChange((latest) => {
@@ -34,21 +34,17 @@ export const PageContent = React.memo(() => {
   
   
   return (<Box as='main' pos='relative' w='100vw'>
-      {/* <Menu sx={{ width: '100%', bg: 'transparent', p: '4'}} /> */}
-      {/* <AdaptiveSpace unit={{sm: '2rem', md: '6rem'}} /> */}
-        <Box 
-          as='section'
-          h='100vh'
-          sx={{
-            position: 'relative',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            maxH: '80vh',
-          }}
-        >
-          <TalksFormCust />
-        </Box>
+      <MotionModal>
+        <Blob contentReplacement={contentReplacement}>
+          <InvitationToTheCave
+            display="flex"
+            alignItems="center"
+            contentReplacement={contentReplacement}
+            setContentReplacement={() => setContentReplacement(!contentReplacement)}
+            onSubmit={() => setContentReplacement(false)}
+          />
+        </Blob>
+      </MotionModal>
     </Box>
   );
 });
