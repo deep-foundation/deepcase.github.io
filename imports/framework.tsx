f7f640254610d5bd303f319f8cdc46d33fae97eb
Aruseli <llingveri@gmail.com>
2023-07-14 17:02:34 +0000

    removed unused components

--- imports/framework.tsx
+++ imports/framework.tsx
@@ -10,41 +10,14 @@ export { useInterval };
 import { Card } from './card';
 export { Card as GravityCard };
 
-import ConfettiExplosion from '@reonomy/react-confetti-explosion';
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react';
-export { default as ConfettiExplosion } from '@reonomy/react-confetti-explosion';
 
 export const useMediaQuery = function useMediaQuery(arg) {
   const [actualValue, isBrowser] = useMediaQueryChakra(arg);
   const [value, setValue] = useState(false);
   useEffect(() => setValue(actualValue), [actualValue, isBrowser]);
   return [value, isBrowser];
-}
-
-export function ButtonExplotion(props: any) {
-  const [exp, setExp] = useState(false);
-  return (
-    <Button {...props} onClick={(e) => {
-      setExp(true);
-      setTimeout(() => setExp(false), 1000);
-      props?.onClick && props?.onClick(e);
-    }} style={{ position: 'relative' }}>
-      {!!exp && <div style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-      }}><ConfettiExplosion {...{
-        force: 0.4,
-        duration: 2000,
-        particleCount: 30,
-        floorHeight: 500,
-        floorWidth: 300,
-        colors: ['#000'],
-      }}/></div>}
-      {props?.children}
-    </Button>
-  )
 }
 
 export const Typography = React.memo(({children}:{children: any}) => {
