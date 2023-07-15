5f0a39c4ecc9bcb272d831083e4aa898bf7b085d
Aruseli <llingveri@gmail.com>
2023-07-15 01:04:30 +0000

    organize imports

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,24 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import detectBrowserLanguage from 'detect-browser-language';
-import React, { useCallback, useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import VisibilitySensor from 'react-visibility-sensor';
-import { Box, Center, Container } from '../imports/framework';
-import { Menu } from '../imports/guild/menu';
-import { MainTasks } from '../imports/guild/main-tasks';
-import { Welcome } from '../imports/guild/welcome';
-import { FlexSection } from '../imports/layout';
+import React, { useEffect } from 'react';
+import { Box } from '../imports/framework';
 import { Provider } from '../imports/provider';
-import { AdaptiveSpace, Space } from '../imports/space';
-import { coreTheme, linksTheme } from '../imports/theme/build';
+import { linksTheme } from '../imports/theme/build';
 
-import { MapText } from '../imports/guild/map-text';
-import { MapSvgDrawCont } from '../imports/icons/mapDrawCont';
-import { AppSS } from '../imports/guild/menu-animation';
 import { useScroll } from 'framer-motion';
-import { Values } from '../imports/guild/values/values';
 import { Documentation } from '../imports/links/documentation';
-// import { motion, useScroll } from "framer-motion";
 
 
 export default function Page () {
