1eb639ea6a261c7f05ff11092b197fbbfeed991a
Aruseli <llingveri@gmail.com>
2023-07-15 01:06:13 +0000

    commonjs to es, updated packages, removed unused packages

--- next.config.js
+++ next.config.js
@@ -7,5 +7,5 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   basePath: isProd ? '' : '',
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deepcase.github.io/main/docs/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deepcase.github.io/main/docs/' : ' ',
 });
