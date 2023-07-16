2b87599df74a2d247094408b95397319d64088f5
Aruseli <llingveri@gmail.com>
2023-07-16 18:37:48 +0000

    replacing require with import

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx$/
-});
+import withMDX from '@next/mdx';
+// const withMDX = require('@next/mdx')({
+//   extension: /\.mdx$/
+// });
 
 const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = withMDX({
+const mdx = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   basePath: isProd ? '' : '',
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deepcase.github.io/main/docs/' : ' ',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deepcase.github.io/main/docs/' : '',
+  swcMinify: true,
 });
+
+export default mdx;
